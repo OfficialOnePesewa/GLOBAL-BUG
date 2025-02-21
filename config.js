652e9de31397abafabc582c6ef268d7fc92b8da2
OfficialOnePesewa <153733681+OfficialOnePesewa@users.noreply.github.com>
2025-02-21 22:44:37 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-global.namaown = "Qasim Ali" // your owner name here
-global.namabot = "© GlobalTechInfo" // your bot name here
+global.namaown = "Official OnePesewa" // your owner name here
+global.namabot = "© OP_TechInfo" // your bot name here
 global.versisc = "9.5.0" //
-global.owner = ["923444844060"] // your owner number here
-global.tele = "https://t.me/GlobalBotInc" // your telegram here
-global.url = "https://www.youtube.com/@GlobalTechInfo" // Your YouTube Here
+global.owner = ["233544482494"] // your owner number here
+global.tele = "https://t.me/OfficialOnePesewa" // your telegram here
+global.url = "https://www.youtube.com/@OfficialOnePesewa" // Your YouTube Here
 global.namastore = "LynnZxD" // UBAH NAMA STORE LU
 global.simbol = "✪" // You Can Change Simbol
 global.wlcm = []
